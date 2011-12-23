61fe0b5738b04fed09cab4b8174243bf7b63432a
Eirik S. Morland <eirik@morland.no>
2011-12-23 14:49:57 +0100

    advanced search, europeana style, query string in search box

---
+++ delving-remote-search-7/delvingsearch.js
@@ -0,0 +1,36 @@
+/**
+ * @file delvingsearch.js
+ *
+ * TODO: Write description
+ */
+
+(function($) {	
+	$(document).ready(function() {
+		var querylength = $('#edit-query').val();
+		var height = $('#edit-query').outerHeight();
+		var offset = $('#edit-query').offset();
+		var append = '<div id="clearsearch"><a href="javascript:void(0)" title="' + Drupal.t('Clear') + '">' + querylength + '</a></div>';
+		$('#edit-query').before(append);
+		var width = $('#clearsearch').width();
+		css = {
+			"width": width+15 + 'px',
+			"line-height": height-8 + 'px',
+		}
+		$('#clearsearch').css(css)
+		$('#clearsearch a').text('X');
+		offset.left = offset.left+2;
+		offset.top = offset.top+3;
+		$('#clearsearch').offset(offset);
+		$('#clearsearch a').live("click", (function() {
+			$('#edit-query').val('');
+			clearsearch = $('#clearsearch').remove();
+			$('#edit-query').focus();
+		}));
+		$('#edit-query').focus(function() {
+			clearsearch = $('#clearsearch').remove();
+		});
+		$('#edit-query').focusout(function() {
+			$('#edit-query').before(clearsearch);
+		});
+	})
+})(jQuery);
