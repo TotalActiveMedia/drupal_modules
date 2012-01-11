2a2045ab19556f80df302fcca45d0f5e4caa1634
Eirik S. Morland <eirik@morland.no>
2012-01-11 14:57:49 +0100

    tuned the "clear search" jQ + added link to isShownBy on thumbnail

--- delving-remote-search-7/delvingsearch.js
+++ delving-remote-search-7/delvingsearch.js
@@ -6,33 +6,35 @@
 
 (function($) {	
 	$(document).ready(function() {
-		var querylength = $('#edit-query').val();
+		var width = $('#edit-query').width();
 		var height = $('#edit-query').outerHeight();
 		var offset = $('#edit-query').offset();
-		var append = '<div id="clearsearch"><a href="javascript:void(0)" title="' + Drupal.t('Clear') + '">' + querylength + '</a></div>';
+		var append = '<div id="clearsearch"><a href="javascript:void(0)" title="' + Drupal.t('Clear') + '">X</a></div>';
 		$('#edit-query').before(append);
 		var css = $('#edit-query').css('font-size');
 		$('#clearsearch').css("font-size", css);
-		var width = $('#clearsearch').width();
 		css = {
-			"width": width + 15 + 'px',
-			"line-height": height-8 + 'px',
+			"width": width + 'px',
+			"line-height": height - 8 + 'px',
 		}
 		$('#clearsearch').css(css);
-		$('#clearsearch a').text('X');
 		offset.left = offset.left+2;
 		offset.top = offset.top+3;
 		$('#clearsearch').offset(offset);
 		$('#clearsearch a').live("click", (function() {
 			$('#edit-query').val('');
-			clearsearch = $('#clearsearch').remove();
+			$('#clearsearch').hide();
 			$('#edit-query').focus();
 		}));
+		$('#clearsearch').click(function() {
+			$('#clearsearch').hide();
+			$('#edit-query').focus();
+		});
 		$('#edit-query').focus(function() {
-			clearsearch = $('#clearsearch').remove();
+			$('#clearsearch').hide();
 		});
 		$('#edit-query').focusout(function() {
-			$('#edit-query').before(clearsearch);
+			$('#clearsearch').show();
 		});
 		var modulepath = Drupal.settings.delvingsearch.modulepath;
 		$('.delvingplacesorting').before('<img class="delvingsortabc" src="' + Drupal.settings.basePath + modulepath +
