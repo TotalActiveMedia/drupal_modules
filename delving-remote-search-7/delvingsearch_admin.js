1f014cbde7a698a84f820ce942e60ce0fad31aaf
zemyatin <eric@delving.eu>
2012-03-13 17:18:08 +0100

    customizable fields, use opacity instead of bg color for visible/hidden indicator

--- delving-remote-search-7/delvingsearch_admin.js
+++ delving-remote-search-7/delvingsearch_admin.js
@@ -4,34 +4,31 @@
  * TODO: Write description
  */
 
-(function($) {	
-	$(document).ready(function() {
-		var select = $('td:not(.tabledrag-hide)').find('select');
-		$.each(select, function() {
-			var val = $(this).val();
-			if (val === 'hidden') {
-				var row = $(this).parent().parent().parent();
-				$(row).css({
-      "background-color": "#39424a",
-      "color": "#fff"
-    });
-			}
-		});
-		$(select).change(function () {
-			var val = $(this).val();
-			var row = $(this).parent().parent().parent();
-			if (val === 'hidden') {
-				$(row).css({
-      "background-color": "#39424a",
-      "color": "#fff"
-    });
-			}
-			else {
-				$(row).css({
-      "background-color": "",
-      "color": ""
-    });
-			}
-		});
-	})
+(function($) {
+    $(document).ready(function() {
+        var select = $('td:not(.tabledrag-hide)').find('select');
+        $.each(select, function() {
+            var val = $(this).val();
+            if (val === 'hidden') {
+                var row = $(this).parent().parent().parent();
+                $(row).css({
+                    "opacity": ".4"
+                });
+            }
+        });
+        $(select).change(function () {
+            var val = $(this).val();
+            var row = $(this).parent().parent().parent();
+            if (val === 'hidden') {
+                $(row).css({
+                    "opacity": ".4"
+                });
+            }
+            else {
+                $(row).css({
+                    "opacity": "1"
+                });
+            }
+        });
+    })
 })(jQuery);
